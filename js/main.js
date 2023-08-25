f8d1e753768bb9c2124f9b129b071b72d8c387d0
David Halkin <halkin.david@gmail.com>
2023-08-25 14:34:44 +0300

    fixbug

--- js/main.js
+++ js/main.js
@@ -27,60 +27,61 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 
 	// open always first tab
-	const modal = new bootstrap.Modal(document.getElementById("user"));
+	const modalUser = document.getElementById("user");
+	if (modalUser) {
+		const modal = new bootstrap.Modal(modalUser);
+		modal._element.addEventListener("show.bs.modal", function () {
+			const navLinks = document.querySelectorAll(".nav-link");
+			const tabsEtc = document.querySelectorAll(".tab-pane");
 
-	modal._element.addEventListener("show.bs.modal", function () {
-		const navLinks = document.querySelectorAll(".nav-link");
-		const tabsEtc = document.querySelectorAll(".tab-pane");
-
-		navLinks.forEach(link => {
-			link.classList.remove("active");
-		});
-
-		tabsEtc.forEach(tab => {
-			tab.classList.remove("active", "show");
-		});
-
-		const firstNavLink = document.querySelector(".nav-link");
-		const firstTab = document.querySelector(firstNavLink.getAttribute("data-bs-target"));
-
-		firstNavLink.classList.add("active");
-		firstTab.classList.add("active", "show");
-	});
-
-	// new custom tab
-	const forgotTabLink = document.getElementById("forgot-tab");
-	const forgotTab = document.getElementById("forgot-tab-pane");
-	const navLinks = document.querySelectorAll(".nav-link");
-	const tabsEtc = document.querySelectorAll(".tab-pane");
-
-	forgotTabLink.addEventListener("click", function () {
-		navLinks.forEach(link => {
-			link.classList.remove("active");
-		});
-
-		tabsEtc.forEach(tab => {
-			tab.classList.remove("active", "show");
-		});
-
-		forgotTabLink.classList.add("active");
-		forgotTab.classList.add("active", "show");
-	});
-
-	navLinks.forEach(link => {
-		link.addEventListener("click", function () {
-			navLinks.forEach(otherLink => {
-				otherLink.classList.remove("active");
+			navLinks.forEach(link => {
+				link.classList.remove("active");
 			});
 
 			tabsEtc.forEach(tab => {
 				tab.classList.remove("active", "show");
 			});
 
-			link.classList.add("active");
-			const targetId = link.getAttribute("data-bs-target");
-			const targetTab = document.querySelector(targetId);
-			targetTab.classList.add("active", "show");
+			const firstNavLink = document.querySelector(".nav-link");
+			const firstTab = document.querySelector(firstNavLink.getAttribute("data-bs-target"));
+
+			firstNavLink.classList.add("active");
+			firstTab.classList.add("active", "show");
 		});
-	});
+		// new custom tab
+		const forgotTabLink = document.getElementById("forgot-tab");
+		const forgotTab = document.getElementById("forgot-tab-pane");
+		const navLinks = document.querySelectorAll(".nav-link");
+		const tabsEtc = document.querySelectorAll(".tab-pane");
+
+		forgotTabLink.addEventListener("click", function () {
+			navLinks.forEach(link => {
+				link.classList.remove("active");
+			});
+
+			tabsEtc.forEach(tab => {
+				tab.classList.remove("active", "show");
+			});
+
+			forgotTabLink.classList.add("active");
+			forgotTab.classList.add("active", "show");
+		});
+
+		navLinks.forEach(link => {
+			link.addEventListener("click", function () {
+				navLinks.forEach(otherLink => {
+					otherLink.classList.remove("active");
+				});
+
+				tabsEtc.forEach(tab => {
+					tab.classList.remove("active", "show");
+				});
+
+				link.classList.add("active");
+				const targetId = link.getAttribute("data-bs-target");
+				const targetTab = document.querySelector(targetId);
+				targetTab.classList.add("active", "show");
+			});
+		});
+	}
 });
